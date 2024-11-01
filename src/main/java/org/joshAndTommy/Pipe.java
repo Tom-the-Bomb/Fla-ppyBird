403bba0ed98359c6718fba3223fa1aa86e493ec9
agent27ace <94245688+agent27ace@users.noreply.github.com>
2024-11-01 17:51:17 -0400

    change board to frame and getters for pipe poss

--- src/main/java/org/joshAndTommy/Pipe.java
+++ src/main/java/org/joshAndTommy/Pipe.java
@@ -1,7 +1,6 @@
 package org.joshAndTommy;
 
 import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.awt.image.ImageObserver;
 
 public class Pipe {
@@ -12,7 +11,7 @@ public class Pipe {
     public static final int SPEED = 3;//positive number for left movement
 
     Pipe(int pipeHeight) {
-        pos = new Point(Board.WIDTH + PIPE_WIDTH ,0);
+        pos = new Point(Frame.WIDTH + PIPE_WIDTH ,0);
         this.pipeHeight = pipeHeight;
     }
     public void draw(Graphics g, ImageObserver observer) {
@@ -25,12 +24,25 @@ public class Pipe {
                 pos.x,
                 pipeHeight + space,
                 PIPE_WIDTH,
-                Board.HEIGHT);
+                Frame.HEIGHT);
         g.setColor(new Color(23, 227, 29));
     }
 
     public void tick() {
         //moves pipe
         pos.translate(-SPEED, 0);
+    }
+
+    public Point getPointTL() {
+        return new Point(pos.x, pipeHeight);
+    }
+    public Point getPointTR() {
+        return new Point(pos.x + PIPE_WIDTH, pipeHeight);
+    }
+    public Point getPointBL() {
+        return new Point(pos.x, pipeHeight + space);
+    }
+    public Point getPointBR() {
+        return new Point(pos.x + PIPE_WIDTH, pipeHeight + space);
     }
 }
